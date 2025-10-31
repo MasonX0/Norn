7f679d894963a43a84d1fad09b9f4bd854332859
admin <ajgizmavletberdin@gmail.com>
2025-11-01 02:03:17 +0500

    update student class

--- composeApp/src/commonMain/kotlin/ru/bpo/norn/commonMain/models/Student.kt
+++ composeApp/src/commonMain/kotlin/ru/bpo/norn/commonMain/models/Student.kt
@@ -1,16 +1,41 @@
 package ru.bpo.norn.commonMain.models
 
 
+
+class Practice (
+    val nameOfOrganization:String,
+    val nameOfHead: String,
+    val city:String,
+    val countOfPlaces:Int
+)
+
+{
+    //private count :Int = countOfPlaces
+    init {
+
+
+
+    }
+
+}
+
+
 class Student(
     val name: String,
 
     //?
     val group: String,
 
+    val isForeign:Boolean,
+
     val gradeForPractice: String,
+
     val nameOfPracticeBase: String,
+
     val typeOfPractice: String,
+
     val periodOfPractice: String,
+
     val formOfPractice: String,
     val withPayment: Boolean,
 
